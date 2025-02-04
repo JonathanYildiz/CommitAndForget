0221578603449613ce49814c3d7c3d73f61137ed
Jonathan Yildiz <jonathan.yildiz@bs-erlangen.de>
2025-02-04 11:44:47 +0100

    Model angelegt

---
+++ Model/BenutzerModel.cs
@@ -0,0 +1,63 @@
+﻿using CommitAndForget.Essentials;
+
+namespace CommitAndForget.Model
+{
+  public class BenutzerModel : NotifyObject
+  {
+    #region Properties
+    public int Key
+    {
+      get => Get<int>();
+      set => Set(value);
+    }
+
+    public string Name
+    {
+      get => Get<string>();
+      set => Set(value);
+    }
+
+    public string Strasse
+    {
+      get => Get<string>();
+      set => Set(value);
+    }
+
+    public int Hausnummer
+    {
+      get => Get<int>();
+      set => Set(value);
+    }
+
+    public string Postleitzahl
+    {
+      get => Get<string>();
+      set => Set(value);
+    }
+
+    public string Wohnort
+    {
+      get => Get<string>();
+      set => Set(value);
+    }
+
+    public string Mail
+    {
+      get => Get<string>();
+      set => Set(value);
+    }
+
+    public string Passwort
+    {
+      get => Get<string>();
+      set => Set(value);
+    }
+
+    public bool IsAdmin
+    {
+      get => Get<bool>();
+      set => Set(value);
+    }
+    #endregion Properties
+  }
+}
