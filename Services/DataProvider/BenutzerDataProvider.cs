cf4be5448df2d451b5b98aca0788a79bb0d6cb34
Jonathan Yildiz <jonathan.yildiz@bs-erlangen.de>
2025-02-04 12:28:15 +0100

    - Laden der Benutzer implementiert
    - LoginViewModel angefangen

---
+++ Services/DataProvider/BenutzerDataProvider.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.ObjectModel;
+using System.Data;
+using System.Windows;
+using CommitAndForget.Model;
+
+namespace CommitAndForget.Services.DataProvider
+{
+  public static class BenutzerDataProvider
+  {
+    public static ObservableCollection<BenutzerModel> LoadBenutzer()
+    {
+      try
+      {
+        DataTable dt = DataBaseService.ExecuteSP("spLoadBenutzer");
+        var benutzerListe = new ObservableCollection<BenutzerModel>();
+
+        if (dt is not null && dt.Rows.Count > 0)
+        {
+          foreach (DataRow row in dt.Rows)
+          {
+            var benutzer = new BenutzerModel();
+
+            benutzer.Key = row["nKey"] != DBNull.Value ? (int)row["nKey"] : default;
+            benutzer.Name = row["szName"] != DBNull.Value ? row["szName"].ToString() ?? string.Empty : string.Empty;
+            benutzer.Strasse = row["szStrasse"] != DBNull.Value ? row["szStrasse"].ToString() ?? string.Empty : string.Empty;
+            benutzer.Hausnummer = row["nHausnummer"] != DBNull.Value ? (int)row["nHausnummer"] : default;
+            benutzer.Postleitzahl = row["szPostleitzahl"] != DBNull.Value ? row["szPostleitzahl"].ToString() ?? string.Empty : string.Empty;
+            benutzer.Wohnort = row["szWohnort"] != DBNull.Value ? row["szWohnort"].ToString() ?? string.Empty : string.Empty;
+            benutzer.Mail = row["szMail"] != DBNull.Value ? row["szMail"].ToString() ?? string.Empty : string.Empty;
+            benutzer.Passwort = row["szPasswort"] != DBNull.Value ? row["szPasswort"].ToString() ?? string.Empty : string.Empty;
+            benutzer.IsAdmin = row["bIsAdmin"] != DBNull.Value ? (bool)row["bIsAdmin"] : default;
+
+            benutzerListe.Add(benutzer);
+          }
+        }
+        return benutzerListe;
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show($"Es ist ein Fehler aufgetreten: {ex.Message}", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+        return [];
+      }
+    }
+  }
+}
--- ViewModel/LoginViewModel.cs
+++ ViewModel/LoginViewModel.cs
@@ -1,22 +1,77 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
 using CommitAndForget.Essentials;
 using CommitAndForget.Model;
 using CommitAndForget.Services.DataProvider;
+using CommunityToolkit.Mvvm.Input;
 
 namespace CommitAndForget.ViewModel
 {
   public class LoginViewModel : NotifyObject
   {
-    ObservableCollection<TestModel> TestList
+    #region Properties
+    public ObservableCollection<BenutzerModel> BenutzerListe
     {
-      get => Get<ObservableCollection<TestModel>>();
+      get => Get<ObservableCollection<BenutzerModel>>();
       set => Set(value);
     }
-    
-    public LoginViewModel()
+
+    public string Mail
     {
-      TestList = TestDataProvider.LoadTestData();
+      get => Get<string>();
+      set => Set(value);
     }
 
+    public string Passwort
+    {
+      get => Get<string>();
+      set => Set(value);
+    }
+    #endregion Properties
+
+    #region Konstruktor
+    public LoginViewModel()
+    {
+      //Alle Benutzer laden
+      BenutzerListe = BenutzerDataProvider.LoadBenutzer();
+      CreateCommands();
+    }
+    #endregion Konstuktor
+
+    #region Commands
+    private void CreateCommands()
+    {
+      LoginCommand = new RelayCommand(Login);
+      RegistrierenCommand = new RelayCommand(Registrieren);
+    }
+    public ICommand LoginCommand { get; private set; }
+    public ICommand RegistrierenCommand { get; private set; }
+    #endregion Commands
+
+    #region Methoden
+    private void Login()
+    {
+      if(string.IsNullOrEmpty(Mail) || string.IsNullOrEmpty(Passwort))
+      {
+        MessageBox.Show("Bitte geben Sie Ihre Mail und Ihr Passwort ein", "Fehler", MessageBoxButton.OK, MessageBoxImage.Warning);
+        return;
+      }
+
+      foreach (var benutzer in BenutzerListe)
+      {       
+        if (benutzer.Mail == Mail && benutzer.Passwort == Passwort)
+        {
+          //Login erfolgreich
+          //TODO JYI Login handlen
+        }
+      }
+    }
+
+    private void Registrieren()
+    {
+      // Registrierung handlen
+    }
+    #endregion Methoden
   }
 }
