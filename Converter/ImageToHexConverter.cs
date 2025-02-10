3e101e510b17e910a6141c9ec7b22749d8b4e73a
Jonathan Yildiz <jonathan.yildiz@bs-erlangen.de>
2025-02-10 12:18:05 +0100

    Testimage verkleinert

---
+++ Converter/ImageToHexConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommitAndForget.Converter
+{
+  public static class ImageToHexConverter
+  {
+    /// <summary>
+    /// Lokales Bild in Hexadezimal-String umwandeln um es in DB-Skripte einfügen zu können
+    /// schreibweise: ...VALUES(X'...')
+    /// </summary>
+    /// <param name="imagePath"></param>
+    /// <returns></returns>
+    public static string ConvertImageToHex(string imagePath)
+    {
+      // 1. Bild als Byte-Array laden
+      byte[] imageBytes = File.ReadAllBytes(imagePath);
+
+      // 2. Byte-Array in einen Hexadezimal-String umwandeln
+      StringBuilder hexString = new StringBuilder(imageBytes.Length * 2);
+      foreach (byte b in imageBytes)
+      {
+        hexString.AppendFormat("{0:X2}", b);
+      }
+
+      // 3. Den Hexadezimal-String zurückgeben
+      return hexString.ToString();
+    }
+  }
+}
