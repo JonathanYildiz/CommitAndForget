cc6f6ebea239594931507dcebcc6b7147758c310
Vita Shelest <vita.shelest@bs-erlangen.de>
2025-02-10 08:41:12 +0100

    spGetMenus und spGetIngredients wurde erstellt

---
+++ SQL/02_StoredProcedures/15_spGetMenus.sql
@@ -0,0 +1,33 @@
+USE dbcommitandforget;
+
+DROP PROCEDURE IF EXISTS spGetMenus;
+
+DELIMITER $$
+-- Laden von Produkten mit Zutaten
+CREATE PROCEDURE spGetMenus()
+
+BEGIN 
+SELECT 
+	m.nKey AS menu_nKey,
+    m.szName AS menu_szName,
+    m.rPrice AS menu_rPrice,
+    m.nImageLink AS menu_nImageLink,
+	p.nKey AS product_nKey,
+    p.szName AS product_szName,
+    p.nEnergy AS product_nEnergy,
+    p.rPrice AS product_rPrice,
+    p.nImageLink AS product_nImageLink,
+    i.nKey AS ingredient_nNey,
+    i.szName AS ingredient_szName
+    FROM tblmenu m
+    LEFT JOIN tblproductmenu pm ON m.nKey = pm.nMenuLink
+    LEFT JOIN tblproduct p ON pm.nProductLink = p.nKey
+    LEFT JOIN tblproductingredient pi ON p.nKey = pi.nProductLink
+    LEFT JOIN tblingredient i ON pi.nIngredientLink = i.nKey
+    ORDER BY m.nKey, p.szName, i.szName;
+END $$
+
+DELIMITER ;
+
+
+
---
+++ SQL/02_StoredProcedures/16_spGetIngredients.sql
@@ -0,0 +1,20 @@
+USE dbcommitandforget;
+
+DROP PROCEDURE IF EXISTS spGetIngredients;
+
+DELIMITER $$
+-- Laden von Produkten mit Zutaten
+CREATE PROCEDURE spGetIngredients()
+
+BEGIN 
+SELECT 
+    i.nKey AS ingredient_nNey,
+    i.szName AS ingredient_szName
+    FROM tblingredient i
+    ORDER BY i.nKey;
+END $$
+
+DELIMITER ;
+
+
+
