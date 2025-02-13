d9560aef25d77db4fb39974c2a793e269098ab4a
Vita Shelest <vita.shelest@bs-erlangen.de>
2025-02-13 09:45:48 +0100

    spGetIngredients wurde angepasst

--- SQL/02_StoredProcedures/16_spGetIngredients.sql
+++ SQL/02_StoredProcedures/16_spGetIngredients.sql
@@ -4,13 +4,17 @@ DROP PROCEDURE IF EXISTS spGetIngredients;
 
 DELIMITER $$
 -- Laden von Produkten mit Zutaten
-CREATE PROCEDURE spGetIngredients()
+CREATE PROCEDURE spGetIngredients(
+p_ProductLink INT
+)
 
 BEGIN 
 SELECT 
     i.nKey AS ingredient_nKey,
-    i.szName AS ingredient_szName
+    i.szName AS ingredient_szName,
+    pi.nQuantity AS ingredient_nQuantity
     FROM tblingredient i
+    LEFT JOIN tblproductingredient pi ON pi.nIngredientLink = i.nKey AND pi.nProductLink = p_ProductLink
     ORDER BY i.nKey;
 END $$
 
