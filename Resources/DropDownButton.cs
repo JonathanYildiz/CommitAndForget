ffaa85497a4a61e6e2a4d6c4e125e8d85255a2ec
Vincent <vincent.meiler@bs-erlangen.de>
2025-03-19 10:11:21 +0100

    navigation button gefixt
    neuer style in ResourceDictionary
    neuer dropdownbutton
    ProfileEditView angelegt

---
+++ Resources/DropDownButton.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows;
+
+namespace CommitAndForget.Resources
+{
+  public class DropDownButton : ToggleButton
+  {
+    // *** Dependency Properties ***
+
+
+    public static readonly DependencyProperty DropDownProperty = DependencyProperty.Register("DropDown", typeof(ContextMenu), typeof(DropDownButton), new UIPropertyMetadata(null));
+
+    // *** Constructors ***
+
+    public DropDownButton()
+    {
+      // Bind the ToogleButton.IsChecked property to the drop-down's IsOpen property
+
+      Binding binding = new Binding("DropDown.IsOpen");
+      binding.Source = this;
+      this.SetBinding(IsCheckedProperty, binding);
+    }
+
+    // *** Properties ***
+
+    public ContextMenu DropDown
+    {
+      get
+      {
+        return (ContextMenu)GetValue(DropDownProperty);
+      }
+      set
+      {
+        SetValue(DropDownProperty, value);
+      }
+    }
+
+    // *** Overridden Methods ***
+
+    protected override void OnClick()
+    {
+      if (DropDown != null)
+      {
+        // If there is a drop-down assigned to this button, then position and display it
+
+        DropDown.PlacementTarget = this;
+        DropDown.Placement = PlacementMode.Bottom;
+
+        DropDown.IsOpen = true;
+      }
+    }
+  }
+}
